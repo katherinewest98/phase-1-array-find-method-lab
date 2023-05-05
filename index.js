f8d771271834770538f34584b154304711dcf885
Katherine West <katherinewest98@yahoo.com>
2023-05-05 07:44:43 -0500

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+/*function winFinder(object) {
+    if (object.result === "W") {
+        return true;
+    } else {
+        return false;
+    }
+}*/
+
+ /* function superbowlWin(record) {
+    const result = record.find(winFinder);
+    return result ? result.year : undefined;
+}*/
+
+function superbowlWin(array) {
+    const result = array.find(winFinder => winFinder.result === "W");
+    return result ? result.year : undefined;
+}
+
+console.log(superbowlWin(record));
